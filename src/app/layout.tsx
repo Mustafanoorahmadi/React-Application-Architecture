4ec22e09f6d2988e8c8fe6a51ea7684029c9c154
Mostafa <noorahmadimustafa204@gmail.com>
2024-08-31 16:10:47 +0430

    set fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,43 @@
 import "./globals.css";
 
+import { Figtree } from "@next/font/google";
+import localFont from "@next/font/local";
+
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-figtree",
+});
+
+// need new font for 'fa' langoage
+const Iranian = localFont({
+  src: [
+    {
+      path: "../../public/font/Iranian/IranianSans.otf",
+      weight: "100",
+      style: "normal",
+    },
+  ],
+  variable: "--font-Iranian",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="rtl">
+    <html dir="rtl" className={`${figtree.variable} ${Iranian.variable}`}>
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className="bg-gray-300 flex items-center justify-center h-20">
-          HEADER
+          دوره معماری ریکت
         </header>
         <div className="flex-1 flex">{children}</div>
-        <footer className="flex bg-gray-300 items-center justify-center
-         h-20">
+        <footer
+          className="flex bg-gray-300 items-center justify-center
+         h-20"
+        >
           FOOTER
         </footer>
       </body>
