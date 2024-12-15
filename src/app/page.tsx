d53d842bc172c8d81cf051244a85e59b2d588938
Mostafa <noorahmadimustafa204@gmail.com>
2024-12-15 22:23:02 +0330

    add : fetch api courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import { CourseSummary } from "@/types/course-summary.interface";
 import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
 
-export default function Home() {
+async function getNewesCourses(count: number): Promise<CourseSummary[]> {
+  
+  const res = await fetch(`https://api.classbon.com/api/courses/newest/${count}`, {
+    next: {
+      revalidate: 24 * 60 * 60
+    }
+  });
+  return res.json();
+}
+
+export default async function Home() {
+  const newestCourses = await getNewesCourses(4)
   return (
     <>
-     <HomeHeroSection />
+      <HomeHeroSection />
+      {
+        newestCourses.map(p => (
+          <p key={p.title}>{p.title}</p>
+        ))
+      }
     </>
   );
 }
